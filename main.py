0bef3c1004236fa098d722b6c411d591ebe5a982
togawa427 <ssinya0922@gmail.com>
2021-12-09 16:30:29 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ from diaries.DiarySample import DiarySample
 from diaries.KameiDiary import KameiDiary
 from diaries.NaruseDiary import NaruseDiary
 from diaries.RenDiary import RenDiary
+from diaries.HayasiDiary import HayasiDiary
+from diaries.KatoDiary import KatoDiary
+from diaries.IchihashiDiary import IchihashiDiary
+
 
 
 

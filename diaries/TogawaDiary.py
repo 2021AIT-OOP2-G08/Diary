9183bebae5cee6565086ab2c7842c6e0b5e23f19
togawa427 <ssinya0922@gmail.com>
2021-12-09 16:33:51 +0900

    Update TogawaDiary.py

--- diaries/TogawaDiary.py
+++ diaries/TogawaDiary.py
@@ -1,6 +1,6 @@
 from diaries.AbstractDiary import AbstractDiary
 
-class IchihashiDiary(AbstractDiary):
+class TogawaDiary(AbstractDiary):
     
     def get_date(self):
         return "2021-12-09"

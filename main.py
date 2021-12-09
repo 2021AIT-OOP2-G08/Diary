9d3589a1730b1c540a972cacbd5a54fdba40da0e
k20098林直希 <saru.884.nonsk.zamurai@gmail.com>
2021-12-09 15:56:47 +0900

    日記追加した

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from diaries.DiarySample import DiarySample
+from diaries.HayasiDiary import HayasiDiary
 
 # ↓のリストには、メンバーの各日記が格納されます。
 diaries = [
   DiarySample(),
+  HayasiDiary(),
 ] 
 
 for d in diaries:
